ed7b5a0fdc05da3db78dd9483aa3bd675e356163
Dipanshu Mishra <dipanshu.a.mishra06@gmail.com>
2025-01-29 09:20:33 +0530

    Adding files

---
+++ borderify.js
@@ -0,0 +1,5 @@
+console.log("My First Extension");
+document.body.style.borderLeft = "5px dashed red";
+document.body.style.borderRight = "5px dashed blue";
+document.body.style.borderTop = "5px dashed yellow";
+document.body.style.borderBottom = "5px dashed green";
